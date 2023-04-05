f09cfd3b3fdce2665aa9580d40f5c3a2a9a2877c
Soulaimane yahya <soulaimaneyh07@gmail.com>
2023-04-05 15:54:18 +0000

    feat - graphql yoga

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('graphql developer')
+import { createSchema, createYoga } from 'graphql-yoga'
+import { createServer } from 'node:http'
+
+const yoga = createYoga({
+    schema: createSchema({
+        typeDefs: /* GraphQL */ `
+            type Query {
+                greeting: String!
+            }
+        `,
+        resolvers: {
+            Query: {
+                greeting: () => 'graphql developer'
+            }
+        }
+    })
+})
+
+const server = createServer(yoga)
+
+server.listen(3000, () => {
+    console.info('Server is running on http://localhost:3000/graphql')
+})
