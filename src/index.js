bf401821fe619346caf8a920612b29342478e481
Soulaimane yahya <soulaimaneyh07@gmail.com>
2023-04-08 05:26:44 +0000

    feat - one to many relationship

--- src/index.js
+++ src/index.js
@@ -8,10 +8,18 @@ const yoga = createYoga({
     schema: createSchema({
         typeDefs: /* GraphQL */ `
             type Query {
+                user(id: ID!): User!
+                users(id: ID, name: String, sort: String, order: String, page: Int, limit: Int): [User!]!
                 product(id: ID!): Product!
                 products(id: ID, status: String, sort: String, order: String, page: Int, limit: Int): [Product!]!
                 category(id: ID!): Category!
                 categories(id: ID, name: String, sort: String, order: String, page: Int, limit: Int): [Category!]!
+            }
+
+            type User {
+                id: ID!
+                name: String!
+                products: [Product!]
             }
 
             type Product {
@@ -21,17 +29,33 @@ const yoga = createYoga({
                 price: Int!
                 quantity: Int
                 status: String!
-                category: Category!
+                user: User!
+                category: Category
             }
 
             type Category {
                 id: ID!,
                 name: String!
                 description: String!
+                products: [Product!]
             }
         `,
         resolvers: {
             Query: {
+                user: async (parent, args, context, info) => (await axios.get(`${url}/users/${args.id}`)).data,
+                users: async (parent, args, context, info) => {
+
+                    let id = args.id != null ? `id=${args.id}` : '';
+                    let name = args.name != null ? `name=${args.name}` : '';
+
+                    let sort = args.sort != null ? `_sort=${args.sort}` : '';
+                    let order = args.order != null ? `_order=${args.order}` : '';
+                    let page = args.page != null ? `_page=${args.page}` : '';
+                    let limit = args.limit != null ? `_limit=${args.limit}` : '';
+
+                    return (await axios.get(`${url}/users?${id}&${name}&${sort}&${order}&${page}&${limit}`)).data
+                },
+
                 product: async (parent, args, context, info) => (await axios.get(`${url}/products/${args.id}`)).data,
                 products: async (parent, args, context, info) => {
 
@@ -62,7 +86,11 @@ const yoga = createYoga({
             },
             Product: {
                 // whenever i go to products, fire category fct
-                category: async (parent, args, context, info) => (await axios.get(`${url}/categories/${parent.category}`)).data
+                category: async (parent, args, context, info) => (await axios.get(`${url}/categories/${parent.category}`)).data,
+                user: async (parent, args, context, info) => (await axios.get(`${url}/users/${parent.user}`)).data
+            },
+            Category: {
+                products: async (parent, args, context, info) => (await axios.get(`${url}/products?category=${parent.id}`)).data
             }
         }
     })
